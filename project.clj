1c48b59f10eab2461a36dd196df20ab2acdac0c7
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2017-09-09 11:40:22 -0300

    Set min lein version.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
    :description "FIXME: write description"
    :dependencies [[org.clojure/clojure "1.8.0"]
                   [metosin/compojure-api "1.1.11"]]
+   :min-lein-version "2.0.0"
    :ring {:handler dice-api.handler/app}
    :uberjar-name "server.jar"
    :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]
