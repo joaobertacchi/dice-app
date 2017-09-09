317da10a6dbd0001a95bcfd286492d9e7b785936
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2017-09-09 13:35:44 -0300

    Fix cheshire and commons-codec dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
  (defproject dice-api "0.1.0-SNAPSHOT"
    :description "FIXME: write description"
    :dependencies [[org.clojure/clojure "1.8.0"]
-                  [metosin/compojure-api "1.1.11"]
+                  [metosin/compojure-api "1.1.11" :exclusions [cheshire commons-codec]]
                   [ring/ring-jetty-adapter "1.6.2"]
                   [environ "1.0.0"]]
    :min-lein-version "2.0.0"
