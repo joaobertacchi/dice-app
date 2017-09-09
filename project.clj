34e08bd07b870f563149d0a3d3d5ed82084508ff
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2017-09-09 13:18:21 -0300

    Fix ring addapter version

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
    :description "FIXME: write description"
    :dependencies [[org.clojure/clojure "1.8.0"]
                   [metosin/compojure-api "1.1.11"]
-                  [ring/ring-jetty-adapter "1.4.0"]
+                  [ring/ring-jetty-adapter "1.6.2"]
                   [environ "1.0.0"]]
    :min-lein-version "2.0.0"
    :plugins [[environ/environ.lein "0.3.1"]]
