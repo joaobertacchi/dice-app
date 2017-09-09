905bd9294b3485b943df7dd8b18a823fa9c12ff4
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2017-09-09 15:35:28 -0300

    Fix dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
  (defproject dice-api "0.1.0-SNAPSHOT"
    :description "FIXME: write description"
    :dependencies [[org.clojure/clojure "1.8.0"]
-                  [metosin/compojure-api "2.0.0-alpha7"]
+                  [metosin/compojure-api "1.1.11" :exclusions [commons-codec]]
                   [ring/ring-jetty-adapter "1.6.2"]
                   [environ "1.0.0"]]
    :min-lein-version "2.0.0"
